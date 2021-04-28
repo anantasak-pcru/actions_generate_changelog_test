7cf42ee1ac99352c7586560d1c61059278a9a16c
Anantasak <anantasak.non@pcru.ac.th>
2021-04-28 14:30:13 +0700

    update

--- public_apk.py
+++ public_apk.py
@@ -2,24 +2,29 @@ import http.client
 import os
 import requests
 import pprint
+from dotenv import load_dotenv
+
+load_dotenv()
 
 repositoty = os.environ["REPOSITORY"]
 tag = os.environ["GITHUB_REF"].split('/')[2]
 body = os.environ["BODY"].split(" ")
-assets = os.environ["ASSETS"]
+assets = os.environ["ASSETS"].split(" ")
 uploadUrl = ""
 releaseId = ""
 token = os.environ["GITHUB_TOKEN"]
 
-create_url = "https://api.github.com/repos/anantasak-pcru/wsm-release/releases"
+create_url = "https://api.github.com/repos/{}/releases".format(repositoty)
+
+print(create_url)
+print("tag " + tag)
 
 body = {
     "accept": "application/vnd.github.v3+json",
-    "owner": "anantasak-pcru",
-    "repo": "wsm-release",
+    "owner": repositoty.split('/')[0],
+    "repo": repositoty.split('/')[1],
     "tag_name": tag,
-    "name": tag,
-    "body": body
+    "name": tag
 }
 
 header = {
@@ -33,18 +38,21 @@ print("🚀🚀 Create relase...")
 res = requests.post(url=create_url,headers=header, json=body)
 
 
-# if(res.status_code == 200):
-print("🎉🎉 Created relase success at " + repositoty)
-uploadUrl = str(res.json()['upload_url']).replace("{?name,label}", "")
-releaseId = str(res.json()['id'])
-# else: exit(0)
+if(res.status_code == 200):
+    print("🎉🎉 Created relase success at " + repositoty)
+    pprint.pprint(res.content)
+    uploadUrl = str(res.json()['upload_url']).replace("{?name,label}", "")
+    releaseId = str(res.json()['id'])
+else: 
+    print("🛑🛑 Can't create relase " + tag)
+    exit(0)
 
 print("🚀🚀 Uploading assets...")
 
 for asset in assets:
     fileName = asset.split('/')[-1]
     url = uploadUrl + "?name=" + fileName
-    files = {"file": (fileName, open(assets, 'rb'), 'multipart/form-data')}
+    files = {"file": (fileName, open(asset, 'rb'), 'multipart/form-data')}
     res = requests.post(url, files=files, headers=header)
     # if(res.status_code == 200):
     print("🎉🎉 Uploading success " + fileName)
